a3292f2e7f6d609c5e70b34d5bdff63c3f72118a
rraiz <raiszadehrayan@gmail.com>
2024-05-17 09:49:24 -0400

    react bootstrap added

--- frontend/movie-critic-react-app/src/App.jsx
+++ frontend/movie-critic-react-app/src/App.jsx
@@ -1,7 +1,15 @@
-
+import Header from './components/ui/Header.jsx'
+import Footer from './components/ui/Footer.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
+    return (
+        <>
+        <Header/>
+        <Footer/>
+        </>
+    );
 }
 
 export default App
---
+++ frontend/movie-critic-react-app/src/components/ui/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return(
+        <footer>
+            <p>Movie Critic &copy; {new Date().getFullYear()}</p>
+        </footer>
+    )
+}
+
+export default Footer;
+
