1c0b8515ab1726ea03934103b1ea37915d1f38ca
rraiz <raiszadehrayan@gmail.com>
2024-05-20 23:18:04 -0400

    Initialized Repositories

--- backend/movie_critic/src/main/java/com/rraiz/movie_critic/repository/ProductionCompanyRepository.java
+++ backend/movie_critic/src/main/java/com/rraiz/movie_critic/repository/ProductionCompanyRepository.java
@@ -1,9 +1,11 @@
 package com.rraiz.movie_critic.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rraiz.movie_critic.model.ProductionCompany;
 
+@Repository
 public interface ProductionCompanyRepository extends JpaRepository<ProductionCompany, Integer>{
 
     
