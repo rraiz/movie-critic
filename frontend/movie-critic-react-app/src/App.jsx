cd0cf7763d5248f52436af589c4d73cb1fffc788
rraiz <raiszadehrayan@gmail.com>
2024-06-01 11:39:56 -0400

    New Frontend Created

--- frontend/movie-critic-react-app/src/App.jsx
+++ frontend/movie-critic-react-app/src/App.jsx
@@ -1,18 +1,11 @@
-import Header from './components/ui/Header.jsx'
-import Footer from './components/ui/Footer.jsx'
-import Home from './pages/Home/Home.jsx';
-import './assets/global.css'
-
 
 function App() {
-  
-    return (
-        <>
-        <Header/>
-        <Home/>
-        <Footer/>
-        </>
-    );
+
+  return (
+    <div>
+      hello world, tailwind css
+    </div>
+  )
 }
 
 export default App
