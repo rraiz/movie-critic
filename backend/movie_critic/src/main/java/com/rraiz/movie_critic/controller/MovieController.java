2615dcca79a4b5d22a91fa574d0457d9ebbbc8ee
rraiz <raiszadehrayan@gmail.com>
2024-06-05 16:19:56 -0400

    Added origin for API access

--- backend/movie_critic/src/main/java/com/rraiz/movie_critic/controller/MovieController.java
+++ backend/movie_critic/src/main/java/com/rraiz/movie_critic/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.rraiz.movie_critic.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.rraiz.movie_critic.util.ResponseUtil;
 
 @RestController
 @RequestMapping(path = "/api/v1/movie")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MovieController {
 
     private final MovieService movieService;
