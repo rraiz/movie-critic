c0c6d6600c03ea5c9da7c4210396af3d045b7b0a
rraiz <raiszadehrayan@gmail.com>
2024-06-16 17:17:13 -0400

    minor fixes

--- backend/movie_critic/src/main/java/com/rraiz/movie_critic/feature/security/service/TokenService.java
+++ backend/movie_critic/src/main/java/com/rraiz/movie_critic/feature/security/service/TokenService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -18,8 +18,8 @@ public class TokenService {
     @Autowired
     private JwtEncoder jwtEncoder;
 
-    @Autowired
-    private JwtDecoder jwtDecoder;
+    //@Autowired
+    //private JwtDecoder jwtDecoder;
 
     public String generateJwt(Authentication auth) {
 
