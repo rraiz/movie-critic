2a347207ecdd669e2ceb770ce1090626e41eba3b
rraiz <raiszadehrayan@gmail.com>
2024-07-21 22:38:23 -0400

    Fixed cors mapping

--- backend/movie_critic/src/main/java/com/rraiz/movie_critic/configuration/WebConfig.java
+++ backend/movie_critic/src/main/java/com/rraiz/movie_critic/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package com.rraiz.movie_critic.configuration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -7,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
